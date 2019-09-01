5a3b5f05cd9125fba24afb9856649f5bebe262f7
DonnKey <donnterry@gmail.com>
2019-09-01 15:28:30 -0700

    Disable (with comments) Flurry.

--- app/src/main/java/com/donnKey/aesopPlayer/analytics/StatsLogger.java
+++ app/src/main/java/com/donnKey/aesopPlayer/analytics/StatsLogger.java
@@ -1,8 +1,9 @@
 package com.donnKey.aesopPlayer.analytics;
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import androidx.annotation.NonNull;
+/*
+import android.content.res.AssetManager;
 import androidx.annotation.Nullable;
 
 import com.flurry.android.FlurryAgent;
@@ -12,14 +13,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+ */
 import java.util.Map;
 
+// Flurry is a product for gathering customer demographic data.
+// Currently Flurry is disabled rather than deal with the legal issues involved.
+// All functions are no-ops. No Flurry account exists.
+// There is no assets/api_keys/flurry in GIT; one would need to be created after siging up with Flurry
+// App build.gradle would also need to be uncommented
 class StatsLogger {
 
     private static final String FLURRY_API_KEY_ASSET = "api_keys/flurry";
     private boolean isFlurryEnabled;
 
     StatsLogger(@NonNull Context context) {
+        /*
         String flurryKey = getFlurryKey(context.getAssets());
         if (flurryKey != null && VersionUtil.isOfficialVersion()) {
             new FlurryAgent.Builder()
@@ -27,27 +35,35 @@ class StatsLogger {
                     .build(context, flurryKey);
             isFlurryEnabled = true;
         }
+         */
     }
 
     void logEvent(@NonNull String eventName) {
+        /*
         if (isFlurryEnabled) {
             FlurryAgent.logEvent(eventName);
         }
+         */
     }
 
     void logEvent(@NonNull String eventName, @NonNull Map<String, String> eventData) {
+        /*
         if (isFlurryEnabled) {
             FlurryAgent.logEvent(eventName, eventData);
         }
+         */
     }
 
     @SuppressWarnings("SameParameterValue")
     void endTimedEvent(@NonNull String eventName) {
+        /*
         if (isFlurryEnabled) {
             FlurryAgent.endTimedEvent(eventName);
         }
+         */
     }
 
+    /*
     @Nullable
     private static String getFlurryKey(AssetManager assets) {
         try {
@@ -65,4 +81,5 @@ class StatsLogger {
             return null;
         }
     }
+     */
 }
